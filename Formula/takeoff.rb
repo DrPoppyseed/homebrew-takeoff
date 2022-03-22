bc745c1ddc520484cbf04d73d28e22bb34090130
DrPoppyseed <peaske16180@gmail.com>
2022-03-23 00:06:59 +0900

    fix sha256

--- Formula/takeoff.rb
+++ Formula/takeoff.rb
@@ -2,7 +2,7 @@ class Takeoff < Formula
   desc "Setup Eslint, Prettier, etc... for TS/TSX through an interactive CLI"
   homepage "https://github.com/DrPoppyseed/takeoff"
   url "https://github.com/DrPoppyseed/takeoff/releases/download/v0.1.0-beta/takeoff.tar.gz"
-  sha256 "bff4b122cc2f70fdcd8179a9ea009a436ee4e68bc7671882558fe939bce7a776"
+  sha256 "22dbfcb1560ed701877937f59b8decebd43cb059c2bb46eeedfbee2132203f87"
   license "MIT"
 
   def install
