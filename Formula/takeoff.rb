3dab0421eae3487ceae3d33b5c78c189c77ade79
DrPoppyseed <peaske16180@gmail.com>
2022-03-22 17:07:58 +0900

    move formula file to /Formula

--- Formula/takeoff.rb
+++ Formula/takeoff.rb
@@ -1,11 +1,11 @@
 class Takeoff < Formula
   desc "Setup Eslint, Prettier, etc... for TS/TSX through an interactive CLI"
   homepage "https://github.com/DrPoppyseed/takeoff"
-  url "https://github.com/DrPoppyseed/takeoff/archive/refs/tags/v0.1.0-alpha.tar.gz"
+  url "https://github.com/DrPoppyseed/takeoff/releases/download/v0.1.0-alpha/takeoff.tar.gz"
   sha256 "bfab273a3755a6551cbb172fa44c3f6d333cdfb779cbabded11d95c81acaacc1"
   license "MIT"
 
   def install
-    bin.install "bin/takeoff"
+    bin.install "takeoff"
   end
 end
