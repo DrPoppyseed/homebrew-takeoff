0480871e2ad8d137dc158f746e7d716b49919fd0
DrPoppyseed <peaske16180@gmail.com>
2022-03-23 00:00:25 +0900

    bump version to v0.1.0-beta

--- Formula/takeoff.rb
+++ Formula/takeoff.rb
@@ -1,10 +1,9 @@
 class Takeoff < Formula
   desc "Setup Eslint, Prettier, etc... for TS/TSX through an interactive CLI"
   homepage "https://github.com/DrPoppyseed/takeoff"
-  url "https://github.com/DrPoppyseed/takeoff/releases/download/v0.1.0-alpha/takeoff.tar.gz"
+  url "https://github.com/DrPoppyseed/takeoff/releases/download/v0.1.0-beta/takeoff.tar.gz"
   sha256 "bff4b122cc2f70fdcd8179a9ea009a436ee4e68bc7671882558fe939bce7a776"
   license "MIT"
-  version "0.1.0"
 
   def install
     bin.install "takeoff"
